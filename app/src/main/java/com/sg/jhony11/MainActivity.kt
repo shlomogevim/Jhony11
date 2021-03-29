dccbe3365bf06d49524f262fc17821b8739cd607
shlomo <shlomo.gevim@gmail.com>
2021-03-29 09:05:34 +0300

    my first

--- app/src/main/java/com/sg/jhony11/MainActivity.kt
+++ app/src/main/java/com/sg/jhony11/MainActivity.kt
@@ -13,11 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        val tr=1
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-           
+
         }
     }
 

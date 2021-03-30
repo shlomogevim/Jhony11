332e07b08f4272d35d15c022e3ea3416f4c5d6ad
shlomo <shlomo.gevim@gmail.com>
2021-03-30 12:34:36 +0300

    After build activity_main

--- app/src/main/java/com/sg/jhony11/MainActivity.kt
+++ app/src/main/java/com/sg/jhony11/MainActivity.kt
@@ -7,21 +7,73 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import com.sg.jhony11.databinding.ActivityAddThoughtBinding
+import com.sg.jhony11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    var selectedCtegory = FUNNY
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
        //setSupportActionBar(findViewById(R.id.toolbar))
         val tr=1
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+       binding.fab.setOnClickListener { view ->
             val intent= Intent(this,AddThoughtActivity::class.java)
             startActivity(intent)
 
         }
     }
+    fun mainSeriousClick(view: View) {    //its toggle button every press toggle valuep
+        if (selectedCtegory == SERIOUS) {
+            binding.mainSeriousBtn.isChecked = true
+            return
+        }
+        binding.mainFunnyBtn.isChecked = false
+        binding.mainCrazyBtn.isChecked = false
+        binding.mainPopularBtn.isChecked = false
+        selectedCtegory = SERIOUS
 
+    }
+
+    fun mainFunnyClick(view: View) {
+        if (selectedCtegory == FUNNY) {
+            binding.mainFunnyBtn.isChecked = true
+            return
+        }
+        binding.mainSeriousBtn.isChecked = false
+        binding.mainCrazyBtn.isChecked = false
+        binding.mainPopularBtn.isChecked = false
+        selectedCtegory = FUNNY
+    }
+
+    fun mainCrazyClick(view: View) {
+        if (selectedCtegory == CRAZY) {
+            binding.mainCrazyBtn.isChecked = true
+            return
+        }
+        binding.mainSeriousBtn.isChecked = false
+        binding.mainFunnyBtn.isChecked = false
+        binding.mainPopularBtn.isChecked = false
+        selectedCtegory = CRAZY
+
+    }
+    fun mainPopularClick(view: View) {
+        if (selectedCtegory == POPULAR) {
+            binding.mainPopularBtn.isChecked = true
+            return
+        }
+        binding.mainSeriousBtn.isChecked = false
+        binding.mainFunnyBtn.isChecked = false
+        binding.mainFunnyBtn.isChecked = false
+        selectedCtegory = POPULAR
+
+    }
 
 }
